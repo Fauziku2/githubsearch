e0a2fefcde0c16730e07c07b5a06d39e66ca24a9
Fauziku2 <M.fauzi_87@hotmail.com>
2017-02-13 02:05:19 +0800

    added githubService & getUser

--- src/app/components/profile.component.ts
+++ src/app/components/profile.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { GithubService } from '../services/github.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.component.html'
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  constructor(private githubService: GithubService) {
+    this.githubService.getUser().subscribe(user => {
+      console.log(user);
+    });
+  }
+}
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+  private username: string;
+  private client_id = '38eb7f4e63308db99d8f';
+  private client_secret = 'a36ff9e061630436e162bcd158842ff6bdcb8c7a';
+
+  constructor(private http: Http) {
+    console.log('Github Service Ready...');
+    this.username = 'Fauziku2';
+  }
+
+  getUser() {
+    return this.http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret).map(response => response.json());
+  }
+}
